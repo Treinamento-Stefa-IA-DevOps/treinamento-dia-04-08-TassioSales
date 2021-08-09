3ddd30095a8bf249aea61511baaeab3c66542a98
TassioSales <tassio.ljs@gmail.com>
2021-08-09 14:21:34 -0300

    commit final

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,12 +1,29 @@
 import pickle
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title = 'Api Titanic',
+    description = ''
+)
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
-    with open('model/Titanic.pkl', 'rb') as fid: 
+
+async def titanic(Sex:int,Age:float, Lifeboat:int, Pclass:int):
+    with open('model/Titanic.pkl', 'rb') as fid:
         titanic = pickle.load(fid)
+
+    dados = titanic.predict([[Sex, Age, Lifeboat, Pclass]]).tolist()
+
+    try:
+        return {
+            'survived': bool(dados[0]),
+            'status': 200,
+            'message': 'Deu tudo certo \o' if bool(dados[0]) else 'Não sobreviveu',
+        }
+    except Exception:
+        return {
+            'message': 'Estou com algum problema :('
+        }
 
 @app.get('/model')
 def get():
